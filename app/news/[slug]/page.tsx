c96315bbd04f53ff07d84a2ef8435f71ef01ec65
kanny <w24001@osfl.ac.jp>
2025-05-23 12:59:32 +0900

    commit

--- app/news/[slug]/page.tsx
+++ app/news/[slug]/page.tsx
@@ -1,68 +1,26 @@
-// import {notFound} from "next/navigation";
-// import {getNewsDetail} from "@/app/_libs/microcms";
-// import Article from "@/app/_components/Article";
-// import ButtonLink from "@/app/_components/ButtonLink";
-// import styles from "./page.module.css";
-// type Props = {
-//   params: {
-//     slug: string;
-//   };
-//   searchParams?: {
-//     dk?: string;
-//   };
-// };
-// export default async function Page({ params, searchParams = {} }: Props) {
-//   const data = await getNewsDetail(params.slug, {
-//     draftKey: searchParams.dk,
-//   }).catch(notFound);
-
-//   return (
-//     <>
-//       <Article data={data} />
-//       <div className={styles.footer}>
-//         <ButtonLink href="/news">ニュース一覧へ</ButtonLink>
-//       </div>
-//     </>
-//   );
-// }
-import { notFound } from 'next/navigation';
-import type { Metadata } from 'next';
-import { getNewsDetail } from '@/app/_libs/microcms';
-import Article from '@/app/_components/Article';
-import ButtonLink from '@/app/_components/ButtonLink';
-import styles from './page.module.css';
-
+import {notFound} from "next/navigation";
+import {getNewsDetail} from "@/app/_libs/microcms";
+import Article from "@/app/_components/Article";
+import ButtonLink from "@/app/_components/ButtonLink";
+import styles from "./page.module.css";
 type Props = {
-  params: {
+  params: Promise<{
     slug: string;
-  };
-  searchParams: {
+  }>;
+  searchParams?:Promise<{
     dk?: string;
-  };
+  }>;
 };
-
-export async function generateMetadata({
-  params,
-  searchParams,
-}: Props): Promise<Metadata> {
-  const data = await getNewsDetail(params.slug, {
-    draftKey: searchParams.dk,
-  });
-
-  return {
-    title: data.title,
-    description: data.description,
-    openGraph: {
-      title: data.title,
-      description: data.description,
-      images: [data?.thumbnail?.url ?? ''],
-    },
-  };
-}
-
 export default async function Page({ params, searchParams }: Props) {
-  const data = await getNewsDetail(params.slug, {
-    draftKey: searchParams.dk,
+  // const data = await getNewsDetail(params.slug, {
+  //   draftKey: searchParams.dk,
+  // }).catch(notFound);
+
+  const { slug } = await params; // Await params to get slug
+  const { dk } = (await searchParams) ?? {}; // Provide default empty object if undefined
+
+  const data = await getNewsDetail(slug, {
+    draftKey: dk,
   }).catch(notFound);
 
   return (
